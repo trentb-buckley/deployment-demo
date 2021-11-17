0ba5ebd23a0e08085a8e35ba985ec49a03162d19
Trenton Buckley <trentb.buckley@gmail.comtrentb.buckley@gmail.comqq>
2021-11-17 11:18:00 -0700

    Final Commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const path =  require("path")
+
+const app =  express()
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`Slamming on port ${port}`)
+})
+
+
+
+
